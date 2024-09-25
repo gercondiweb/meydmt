5d094a666102582bc37479a91806933e51fe8918
gercondiweb <160808788+gercondiweb@users.noreply.github.com>
2024-09-24 23:39:34 -0500

    Servicios y Rutas 1

--- src/app/transporte/pages/admrecorridos/admrecorridos.component.ts
+++ src/app/transporte/pages/admrecorridos/admrecorridos.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestService } from '../../services/rest.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admrecorridos',
@@ -6,6 +9,113 @@ import { Component } from '@angular/core';
   templateUrl: './admrecorridos.component.html',
   styleUrl: './admrecorridos.component.css'
 })
-export class AdmrecorridosComponent {
+export class AdmrecorridosComponent implements OnInit{
+
+  datosConsulta={
+    opc:''
+  }
+
+  datosRutas={
+    opc:'',
+    vID:0
+  }
+
+  formServicioVehiculo : FormGroup;
+
+  constructor(
+    private router: Router,
+    private restService: RestService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+
+    this.formServicioVehiculo = this.fb.group({
+      idservicio: [0],
+      fecha:[''],
+      hora:[''],
+      estado:[''],
+      origen:[],
+      destino:[],
+      usuarios:[''],
+      id_vehiculo: [0],
+      id_conductor: [],
+      id_cliente:[],
+      id_tiposervicio:[0],
+      id_zona:[0],
+      cdec:[],
+      cuenta:[],
+      comentario:['']
+    });
+    
+    this.cargarmaestros();
+    
+  }
+
+  listClientes:any;
+  listRutas:any;
+  listConductores:any;
+  listVehiculos:any;
+  listTipoServicio:any;
+  listZona:any;
+
+  rutaSeleccionada: string | null = null;
+
+  seleccionarRuta(codigo: string): void {
+    this.rutaSeleccionada = codigo;
+    console.log('Código de ruta seleccionada:', codigo);
+  }
+
+  cargarmaestros(){
+    this.datosConsulta.opc ='CLI';
+    this.restService.getMaestros(this.datosConsulta).subscribe(respuesta=>{
+      console.log(respuesta);
+      this.listClientes = respuesta.body[0];
+    });
+
+    //cargar rutas
+    this.datosRutas.opc='RUTASCLI';
+    this.datosRutas.vID = 1; //id del cliente al que se le quiere registrar el recorrido
+
+    this.restService.consultatransporte(this.datosRutas)
+    .subscribe(respuesta=>{
+      this.listRutas = respuesta.body[0];
+    });
+
+    //tipos de servicio y demas datos maestros del servicio a registrar
+    this.datosConsulta.opc ='TIPSERV';
+    this.restService.getMaestros(this.datosConsulta).subscribe(respuesta=>{
+      console.log(respuesta);
+      this.listTipoServicio = respuesta.body[0];
+    });
+
+    this.datosConsulta.opc ='ZONA';
+    this.restService.getMaestros(this.datosConsulta).subscribe(respuesta=>{
+      console.log(respuesta);
+      this.listZona = respuesta.body[0];
+    });
+
+    //conductor y vehiculo
+    this.datosConsulta.opc ='CONDUCTOR';
+    this.restService.getMaestros('TCN').subscribe(respuesta=>{
+      console.log(respuesta);
+      this.listConductores = respuesta.body[0];
+    });
+
+
+    this.datosConsulta.opc ='VEHICULO';
+    this.restService.getMaestros(this.datosConsulta).subscribe(respuesta=>{
+      console.log(respuesta);
+      this.listVehiculos = respuesta.body[0];
+    });
+  }
+
+  digitar(){
+
+  }
+
+  regresar(){
+    this.router.navigateByUrl('/transporte/recorridos');
+  }
 
 }
--- src/app/transporte/pages/recorridos/recorridos.component.ts
+++ src/app/transporte/pages/recorridos/recorridos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import { RestService } from '../../services/rest.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,13 +12,13 @@ import { RestService } from '../../services/rest.service';
 export class RecorridosComponent implements OnInit{
 
   datosBusqueda={
-    opc :"CNT-TKT",
-    id_cliente :"1",
-    fechainicial :"2024-01-01",
-    fechafinal :"2024-03-30"
+    opc :"ALLSERV",
+    
   }
 
   listDatos: any; datos:any;
+  listServicios:any; servicios:any;
+  colum = ['id', 'fecha', 'hora', 'tipo', 'cliente', 'placa','estado'];
 
   cards: any[] = [
     { title: 'Solicitados', icon: 'commute', value: 0, cols: 1, rows: 1, ischarts: false, tipo: '' },
@@ -25,10 +26,12 @@ export class RecorridosComponent implements OnInit{
     { title: 'Cancelados', icon: 'cancel', value: 0, cols: 1, rows: 1, ischarts: false, tipo: '' },
     { title: 'Ejecutados', icon: 'done', value: 0, cols: 1, rows: 1, ischarts: false, tipo: '' },
     { title: 'Asignados', icon: 'user', value: 0, cols: 1, rows: 1, ischarts: false, tipo: '' },
+    { title: 'Aplazado', icon: 'user', value: 0, cols: 1, rows: 1, ischarts: false, tipo: '' },
   ];
 
   constructor(
-    private _restService: RestService
+    private _restService: RestService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -36,22 +39,34 @@ export class RecorridosComponent implements OnInit{
   }
 
   cargarDatos(){
-    this._restService.getCountTikets(this.datosBusqueda)
+    this.datosBusqueda.opc='COUNTSERV';
+
+    this._restService.consultatransporte(this.datosBusqueda)
     .subscribe(respuesta=>{
       this.listDatos = respuesta;
-      this.datos = this.listDatos.body;
+      this.datos = this.listDatos.body[1][0];
+      console.log(this.listDatos.body);
+      
+      this.cards[0].value = this.datos.Pendientes;
+      this.cards[1].value = this.datos.Iniciados;
+      this.cards[2].value = this.datos.Cancelados;
+      this.cards[3].value = this.datos.Completados;
+      this.cards[4].value = this.datos.Camino;
+      this.cards[5].value = this.datos.Aplazados;
+    });
 
-      this.cards[0].value = this.datos[0][0].servicios_solicitados;
-      this.cards[1].value = this.datos[0][0].servicios_iniciados;
-      this.cards[2].value = this.datos[0][0].servicios_cancelados;
-      this.cards[3].value = this.datos[0][0].servicios_ejecutados;
-      this.cards[4].value = this.datos[0][0].servicios_asignados;
+    this.datosBusqueda.opc='ALLSERV';
 
+    this._restService.consultatransporte(this.datosBusqueda)
+    .subscribe(respuesta=>{
+      this.listServicios = respuesta;
+      this.servicios = this.listServicios.body[0];
     });
   }
 
   digitar(){
-
+    const route = 'transporte/admrecorridos/'  ;
+      this.router.navigate([route,'Crear']);
   }
 
   filtrar(estado : string){
