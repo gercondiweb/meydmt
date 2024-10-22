faba22448c6df55dcbf4c9aff48b1d6566aec8b5
gercondiweb <160808788+gercondiweb@users.noreply.github.com>
2024-10-22 08:38:32 -0500

    subiendo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: {
-    //base: 'http://localhost/api',
+    //base: 'http://localhost/api', este es el servicor local
     base:'https://meyd-mt-5dad4c7c009d.herokuapp.com/api',
     sucursal: 'sucursales',
     consultaservicio : 'consultaservicio/serv',
